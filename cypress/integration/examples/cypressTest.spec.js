bf328d5fd1220cdaa743ce63c0d5ef54e1feffc8
Mirza Music <nino.mirza@gmail.com>
2020-10-29 12:26:48 +0100

    init

---
+++ cypress/integration/examples/cypressTest.spec.js
@@ -0,0 +1,13 @@
+describe('should display an error message when you enter wrong login information', function () {
+    before(function () {
+      cy.visit('https://www.netflix.com/')
+    })
+   
+    it('displays an error message', function () {
+      cy.get('a[href="/login"]').click()
+      cy.get('#id_userLoginId').type('test@gmail.com',{force: true})
+      cy.get('#id_password').type('1234',{force: true})
+      cy.get('button[class="btn login-button btn-submit btn-small"]').click()
+      cy.get('div[class="ui-message-contents"]').should('be.visible')
+    })
+   })
